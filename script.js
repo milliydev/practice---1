e4d37683081c842ad4ebc1555a68e9c3405f4d24
milliydev <zohidtojiyev071@gmail.com>
2024-01-08 19:20:12 +0500

    practic

---
+++ script.js
@@ -0,0 +1,10 @@
+function add(){
+    let sarlavha = document.getElementById('title').value;
+    let matn = document.getElementById("text").value;
+     
+   document.getElementById("endtitle").innerHTML = sarlavha;
+   document.getElementById("endText").innerHTML = matn;
+    
+}
+
+ 
